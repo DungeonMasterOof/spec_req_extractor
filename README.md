a6721881aa1969169240ecbc9d5ed1b0928d9930
Cookies272 <142683244+DungeonMasterOof@users.noreply.github.com>
2025-03-27 20:12:31 +0300

    Entire README.md, nodes partition and duplication fix
    Added README.md with information on the project.
    Now nodes are separated into rows to transform huge inconvenient graphs into nicer ones.
    Also now the program doesn't create a node if the equivalent one does already exist.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-### Hello world!
+## Spec data extractor
+The program has multiple modules. The main goal is to extract requirements and build requirements fields from a spec file or a set of spec files (in one directory). There is a Makefile to make working with modules simpler. For now, there are:
+`Makefile, script.sh, task.py`  
+Their work is described below.
+
+### Makefile
+Makefile has options to start the program and to clean everything after its work.
+`run` - just starts the script
+`cleanout` - removes .out files that are products of script.sh
+`cleangraph` - removes build_requires and requires files that are realizations of Graphviz graphs in DOT language
+`cleanpng` - removes .png files (supposed to be build_requires.png and requires.png)  
+
+### script.sh
+A bash script to collect requires and build requires data from spec files. It uses
+`rpmspec -q --buildrequires >> br.out` for requires during build and
+`rpmspec -q --requires >> r.out` for requires during runtime.  
+After that the script launches **task.py**
+
+### task.py
+A python module to take the data from the script and to use Graphviz Python API for  creating graphs that the user will receive. It has a function named **proc** for processing the graph creation:
+`proc(graph, packet_name, filename, verflag)`
+It receives *graph* - Graphviz API class that is used for graph declaration  
+*packet_name* - string that contains packet name. It is written by the user, and is used in graph's root. **Note**: For example, python-3 or LibreOffice packets use multiple spec files. That's why it is important to receive name from the user, because it may be unclear what spec has the right name for the packet.  
+*filename* - name of the file that had been made by the script. Should be synced with graph's purpose (br.out for build requirements, r.out for runtime requirements)
+*verflag* - flag for labeling the edges with the versions of required packets instead of leaving it in the nodes. For example, *cmake-data = 3.28.3-7* with verflag = True will be transformed into **"cmake-data" node** and **"= 3.28.3-7" label** on the edge.  
+Also, if there is a condition for having a requirement, for example, *(cmake-rpm-macros = 3.28.3-7 if rpm-build)*, then the parentheses are removed, and future result depends on verflag, mentioned before.
--- task.py
+++ task.py
@@ -1,4 +1,4 @@
-from graphviz import Digraph, escape
+from graphviz import Digraph, unflatten
 
 
 def proc(graph, packet_name, filename, verflag): # Обработка графа graph с корнем packet_name и узлами из файла filename
@@ -7,35 +7,42 @@ def proc(graph, packet_name, filename, verflag): # Обработка графа
 
     file = open(filename, 'r')
     graph.attr('node', shape='box', color='green')
+
     i = 0
-    for line in file.readlines():
+    names = file.readlines()
+    used = []
+
+    for line in names:
         # line = escape('"' + line + '"')
+        if line not in used:
+            used.append(line) # Использованное имя
+            line = line.strip()
+            if (line.startswith("(")) and (line.endswith(")")): # Удаляем некрасивые скобки при условиях
+                line = line[1:-1]
 
-        line = line.strip()
-        if (line.startswith("(")) and (line.endswith(")")): # Удаляем некрасивые скобки при условиях
-            line = line[1:-1]
+            if verflag: # Хотим указывать версии на рёбрах
+                ind = line.find(' ') # Версии зависимостей указывают после пробела
+                if ind != -1: # Указана версия
+                    version = line[(ind+1):] # Запоминаем версию
+                    line = line[:ind] # Забываем обо всём до пробела (включительно)
+                    graph.node(str(i), line)
+                    graph.edge('packet', str(i), version) # Делаем пометку на ребре о версии
+                else: # Версия не указана
+                    graph.node(str(i), line)
+                    graph.edge('packet', str(i))
 
-        if verflag: # Хотим указывать версии на рёбрах
-            ind = line.find(' ') # Версии зависимостей указывают после пробела
-            if ind != -1: # Указана версия
-                version = line[(ind+1):] # Запоминаем версию
-                line = line[:ind] # Забываем обо всём до пробела (включительно)
-                graph.node(str(i), line)
-                graph.edge('packet', str(i), version) # Делаем пометку на ребре о версии
-            else: # Версия не указана
+            else:
                 graph.node(str(i), line)
                 graph.edge('packet', str(i))
 
-        else:
-            graph.node(str(i), line)
-            graph.edge('packet', str(i))
-
-        i += 1
+            i += 1
 
     graph.attr(rankdir='LR')
+    numrow = round(len(used) / 10)# Количество рядов в отображении
+    out = graph.unflatten(stagger = numrow)
 
     # Выводим граф в файл и на экран
-    output_path = graph.render(filename=graph.name, view=True)  # Создаём в качестве файла наш граф
+    output_path = out.render(filename=f'{graph.name}', view=True)  # Создаём в качестве файла наш граф
     print("Был создан граф:", output_path)
 
 
