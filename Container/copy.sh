b57477dee9c09ec1c31f740c640a54bc3bbc0757
Cookies272 <142683244+DungeonMasterOof@users.noreply.github.com>
2025-04-02 03:10:07 +0300

    Fixing commit
    That's what I meant. Well, deviding into 2 paths: direct or containers. Everything will be in README

--- Container/copy.sh
+++ Container/copy.sh
@@ -2,11 +2,14 @@
 # Copying png and graphs from "fedora" Docker container
 # MUST BE LAUNCHED BY ROOT USER
 
+# Change if you use another distributive
+container_name=fedora
+
 echo "Processing cp from container..."
-docker cp fedora:/app/build_requires.png /tmp
-docker cp fedora:/app/runtime_requires.png /tmp
-docker cp fedora:/app/build_requires /tmp
-docker cp fedora:/app/runtime_requires /tmp
+docker cp ${container_name}:/app/build_requires.png /tmp
+docker cp ${container_name}:/app/runtime_requires.png /tmp
+docker cp ${container_name}:/app/build_requires /tmp
+docker cp ${container_name}:/app/runtime_requires /tmp
 
 chmod 666 /tmp/snap-private-tmp/snap.docker/tmp/*
 mv /tmp/snap-private-tmp/snap.docker/tmp/*.png ./
